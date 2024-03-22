91b369d3b0c855f1f217d297f07fe9ef6b82b593
syeef <syeefislam@yahoo.com>
2024-03-22 20:11:30 +0530

    added remove button on cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,31 +5,22 @@ import ReactDOM from "react-dom";
 const cartElements = [
   {
     title: "Colors",
-
     price: 100,
-
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-
     quantity: 2,
   },
 
   {
     title: "Black and white Colors",
-
     price: 50,
-
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-
     quantity: 3,
   },
 
   {
     title: "Yellow and Black Colors",
-
     price: 70,
-
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-
     quantity: 1,
   },
 ];
@@ -48,19 +39,35 @@ function Cart({ isOpen, onClose }) {
           width: "300px",
         }}
       >
-        <Modal.Header closeButton onClick={onClose}>
-          <Modal.Title>Modal Title</Modal.Title>
+        <Modal.Header>
+          <Modal.Title>Cart Items</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <h1>
-            {cartElements.map((item) => (
-              <div key={item.title}>
+          {cartElements.map((item) => (
+            <div
+              key={item.title}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginBottom: "10px",
+              }}
+            >
+              <div>
                 <h3>{item.title}</h3>
-                <h3>{item.price}</h3>
+                <p>Price: ${item.price}</p>
               </div>
-            ))}
-          </h1>
+              <div>
+                <Button
+                  variant="danger"
+                  onClick={() => handleRemoveItem(item.title)}
+                >
+                  Remove
+                </Button>
+              </div>
+            </div>
+          ))}
         </Modal.Body>
 
         <Modal.Footer>
